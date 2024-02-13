1363521b2c324d52d45d51cb7bf04e34f93b3ac8
Bedizz <turkyilmaz.bediz@gmail.com>
2024-02-13 22:37:52 +0100

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import './header.css'
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
+      <div className='header-all-container'>
     <div className="header-container">
-      <img src="/header-removebg-preview.png" alt="pokemon" />
+      <Link to={"/"}><img src="/header-removebg-preview.png" alt="pokemon" /></Link>
       
+    </div>
     </div>
   )
 }
