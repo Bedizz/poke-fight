93f5ca3733fb563a922b4522dd71781512ac1095
Bedizz <turkyilmaz.bediz@gmail.com>
2024-02-14 15:11:58 +0100

    some css

--- src/views/PokemonId.jsx
+++ src/views/PokemonId.jsx
@@ -1,6 +1,32 @@
+import React from 'react'
+import PokemonInfo from '../views/PokemonInfo'
+import { useParams } from "react-router-dom";
+
+export async function loader() {
+  const data = await fetchPokemonData();
+  return data; 
+}
 const PokemonId = () => {
+  const pokemonData = useLoaderData();
+  const {id} = useParams()
+  
+ const pokemon = pokemonData.find(pokemon => {return pokemon.id === id})
+
   return (
-    <div>PokemonId</div>
+    <>
+      {pokemonData && (
+        <>
+          <div className='top-container'>
+            <h1>PokemonName</h1>
+            <img src="Image" alt="PokemonName" />
+            <div>Its power type</div>
+          </div>
+          <div className='bottom-container'>
+            <PokemonInfo />
+          </div>
+        </>
+      )}
+    </>
   )
 }
 
--- src/views/PokemonInfo.jsx
+++ src/views/PokemonInfo.jsx
@@ -1,6 +1,29 @@
+
 const PokemonInfo = () => {
   return (
-    <div>PokemonInfo</div>
+    <div>
+      <div>
+        </div>
+        <div>
+          <div className="abilities">
+            <div>Abilities</div>
+            <div>Ability 1</div>
+            <div>Ability 2</div>
+          </div>
+          <div className="stats">
+            <div>Stats</div>
+            <div>Hp:</div>
+            <div>Attack:</div>
+            <div>Defense:</div>
+            <div>Special-attack:</div>
+            <div>Special-defense:</div>
+            <div>Speed:</div>
+          </div>
+
+
+        </div>
+    
+    </div>
   )
 }
 
